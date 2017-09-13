5395bbb1b353b2fff36efd8a5072c6b695053b01
Abdel K. Bokharouss <a.k.bokharouss@student.tue.nl>
2017-09-13 12:39:11 +0200

    Added elementary operations counters (+ getters)

--- src/Karatsuba.java
+++ src/Karatsuba.java
@@ -4,14 +4,27 @@
  */
 public class Karatsuba extends AbstractSolver {
     
+    private int numberOfElemOperations; // track the number of elementary additions/substractions
+    private int numberOfElemMultiplications; // track the number of elementart multiplications
+    
     public Karatsuba (IntegerRep x, IntegerRep y) {
         super(x, y);
+        this.numberOfElemOperations = 0;
+        this.numberOfElemMultiplications = 0;
     }
     
     @Override
     public IntegerRep compute() {
         // compute with x and y
         return new IntegerRep(0, false, new int[] {0}); // dummy instance
+    }
+    
+    public int getNumberElemOperations() {
+        return this.numberOfElemOperations;
+    }    
+    
+    public int getNumberMultiplications() {
+        return this.numberOfElemMultiplications;
     }
     
 }
--- src/PrimMultiplication.java
+++ src/PrimMultiplication.java
@@ -4,8 +4,13 @@
  */
 public class PrimMultiplication extends AbstractSolver {
     
+    private int numberOfElemOperations; // track the number of elementary additions/substractions
+    private int numberOfElemMultiplications; // track the number of elementart multiplications
+    
     public PrimMultiplication (IntegerRep x, IntegerRep y) {
         super(x, y);
+        this.numberOfElemOperations = 0;
+        this.numberOfElemMultiplications = 0;
     }
     
     @Override
@@ -14,4 +19,11 @@ public class PrimMultiplication extends AbstractSolver {
         return new IntegerRep(0, false, new int[] {0}); // dummy instance
     }
     
+    public int getNumberElemOperations() {
+        return this.numberOfElemOperations;
+    }    
+    
+    public int getNumberMultiplications() {
+        return this.numberOfElemMultiplications;
+    }
 }
