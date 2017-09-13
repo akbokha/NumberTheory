2d8f1c5c1a90042abb2447e2b55709c74736f38a
Remco <remco@192.168.2.81>
2017-09-13 16:39:06 +0200

    Updated Subtraction to work with the new IntegerRep, and fixed handling of negative inputs and outputs, as well as leading 0s.

--- src/Subtraction.java
+++ src/Subtraction.java
@@ -1,56 +1,89 @@
+
 /**
  *
  * @author Abdel K. Bokharouss
  */
 public class Subtraction extends AbstractSolver {
-    
-    public Subtraction (IntegerRep x, IntegerRep y) {
+
+    public Subtraction(IntegerRep x, IntegerRep y) {
         super(x, y);
     }
-    
+
     @Override
     public IntegerRep compute() {
-        if(x.getLength() != y.getLength()){
-            if(x.getLength() > y.getLength()){
+        
+        //handle negative x and y
+        if(x.isNegative()){
+            for(int i = 0; i < x.getLength(); i++){
+                x.getChars()[i] *= -1;
+            }
+        }
+        if(y.isNegative()){
+            for(int i = 0; i < y.getLength(); i++){
+                y.getChars()[i] *= -1;
+            }
+        }
+        
+        
+        //If x and y do not have the same length, add leading 0s to the shortest
+        if (x.getLength() != y.getLength()) {
+            if (x.getLength() > y.getLength()) {
                 int[] temp = new int[x.getLength()];
-                int[] zeros = new int [x.getLength() - y.getLength()];
-                for (int i = 0; i < zeros.length; i++){
+                int[] zeros = new int[x.getLength() - y.getLength()];
+                for (int i = 0; i < zeros.length; i++) {
                     zeros[i] = 0;
                 }
                 System.arraycopy(zeros, 0, temp, 0, zeros.length);
-                System.arraycopy(y, 0, temp, zeros.length, y.getLength());
-                y = temp;
-            }
-            else{
+                System.arraycopy(y.getChars(), 0, temp, zeros.length, y.getLength());
+                y.setChars(temp);
+            } else {
                 int[] temp = new int[y.getLength()];
-                int[] zeros = new int [y.getLength() - x.getLength()];
-                for (int i = 0; i < zeros.length; i++){
+                int[] zeros = new int[y.getLength() - x.getLength()];
+                for (int i = 0; i < zeros.length; i++) {
                     zeros[i] = 0;
                 }
                 System.arraycopy(zeros, 0, temp, 0, zeros.length);
-                System.arraycopy(x, 0, temp, zeros.length, x.getLength());
-                x = temp;
+                System.arraycopy(x.getChars(), 0, temp, zeros.length, x.getLength());
+                x.setChars(temp);
             }
         }
-        int[] solution = new int[x.getLength()]; //System.out.println("Solution length: " + solution.length);
-        for(int i = solution.length - 1; i >= 0; i--){
-            if (x[i] >= y[i]){
-                solution[i] = x[i] - y[i];
+        
+        //Create the solution
+        IntegerRep solution = new IntegerRep(x.getRadix(), false, new int[x.getLength()]);
+        
+        
+        for (int i = solution.getLength() - 1; i >= 0; i--) {
+            //If the outcome of this single digit subtraction is positive
+            if (x.getChars()[i] >= y.getChars()[i]) {
+                solution.getChars()[i] = x.getChars()[i] - y.getChars()[i];
+            } 
+            
+            //If the outcome of this single digit subtraction is negative
+            else {
+                
+                //If this is NOT the last digit
+                if (i != 0) {
+                    x.getChars()[i - 1] = x.getChars()[i - 1] - 1;
+                    x.getChars()[i] = x.getChars()[i] + x.getRadix();
+                    solution.getChars()[i] = x.getChars()[i] - y.getChars()[i];
+                }
+                
+                //If this is the last digit
+                else{
+                    solution = new Subtraction(y, x).compute();
+                    solution.setNegative();
+                }
             }
-            else{
-        //TODO: handle negative numbers
-        if (i != 0){
-            x[i-1] = x[i-1] - 1;
         }
-        x[i] = x[i] + radix;
-        solution[i] = x[i] - y[i];
-            }
+        
+        //remove leading 0s
+        while(solution.getChars()[0] == 0){
+            int[] temp = new int[solution.getLength() - 1];
+            System.arraycopy(solution.getChars(), 1, temp, 0, temp.length);
+            solution.setChars(temp);
         }
-        //TODO: remove leading 0's
-        /*while(solution[0] == 0){
-                solution.
-        }*/
-        return new IntegerRep(0, false, new int[] {0}); // dummy solution instance
+        
+        return solution;
     }
-    
+
 }
