0786330a0ac65d6e4beeb9f5f72720ce43d5a513
Remco <remco@192.168.2.81>
2017-09-14 09:32:04 +0200

    Fixed addition to handle inputs with different lengths.

--- src/Addition.java
+++ src/Addition.java
@@ -16,6 +16,30 @@ public class Addition extends AbstractSolver {
     
     @Override
     public IntegerRep compute() {
+        
+        //If x and y do not have the same length, add leading 0s to the shortest
+        if (x.getLength() != y.getLength()) {
+            if (x.getLength() > y.getLength()) {
+                int[] temp = new int[x.getLength()];
+                int[] zeros = new int[x.getLength() - y.getLength()];
+                for (int i = 0; i < zeros.length; i++) {
+                    zeros[i] = 0;
+                }
+                System.arraycopy(zeros, 0, temp, 0, zeros.length);
+                System.arraycopy(y.getChars(), 0, temp, zeros.length, y.getLength());
+                y.setChars(temp);
+            } else {
+                int[] temp = new int[y.getLength()];
+                int[] zeros = new int[y.getLength() - x.getLength()];
+                for (int i = 0; i < zeros.length; i++) {
+                    zeros[i] = 0;
+                }
+                System.arraycopy(zeros, 0, temp, 0, zeros.length);
+                System.arraycopy(x.getChars(), 0, temp, zeros.length, x.getLength());
+                x.setChars(temp);
+            }
+        }
+        
         int[] result = new int[x.getLength() + 1];
         int remainder;
         int remember = 0;
@@ -23,13 +47,17 @@ public class Addition extends AbstractSolver {
         for(int i = x.getLength() - 1; i >= 0; i--) {
             temp = x.getChars()[i] + y.getChars()[i];
             remainder = temp % x.getRadix();
-            result[x.getLength() - i - 1] = remainder + remember;
+            result[i] = remainder + remember;
             remember = temp / x.getRadix();
         }
         if(remember != 0 ) {
             result[0] = remember;
-        }
-        return new IntegerRep(x.getRadix(), resultNegative, result);
+            return new IntegerRep(x.getRadix(), resultNegative, result);
+        } else {
+            int[] result2 = new int[x.getLength()];
+            System.arraycopy(result, 0, result2, 0, x.getLength());
+            return new IntegerRep(x.getRadix(), resultNegative, result2);
+        }    
     }
 
     
