bf6020294f3f6f5de8af75170f5d5e934eb599b6
Remco <remco@192.168.2.81>
2017-09-14 12:24:21 +0200

    First start of multiplication.

--- src/PrimMultiplication.java
+++ src/PrimMultiplication.java
@@ -29,6 +29,9 @@ public class PrimMultiplication extends AbstractSolver {
                 temp2.add(0,0);
             }
             for (int j = y.getLength() - 1; j >= 0; j--){
+                for (int k = y.getLength() - 1; k > j; k--) {
+                    temp2.add(0, 0);
+                }
                 int xy = x.getChars()[i] * y.getChars()[j];
                 numberOfElemMultiplications++;
                 if(temp2.size() <= j + 1 - y.getLength()){
@@ -38,23 +41,40 @@ public class PrimMultiplication extends AbstractSolver {
                     temp2.set(0, temp2.get(0) + xy % x.getRadix());
                     numberOfElemOperations++;
                 }
-                if(x.getChars()[i] * y.getChars()[j] > x.getRadix()){
-                    temp2.add(0, xy - xy % x.getRadix());
+                if(xy > x.getRadix()){
+                    temp2.add(0, (xy - xy % x.getRadix()) / x.getRadix());
                     numberOfElemOperations++;
                 }
             }
             temp.add(0, temp2);
+            
+            //Print digits added
+            String itemString = "";
+            for (int j = 0; j < temp2.size(); j++){
+                itemString += Integer.toString(temp2.get(j), x.getRadix());
+            }
+            System.out.println("digit: " + itemString);
         }
         
         //convert to int arrays
         ArrayList<int[]> intarrays = new ArrayList<>();
         for(int j = 0; j < temp.size(); j++){
-            int[] result = new int[temp.get(0).size()];
-            for (int i = 0; i < temp.get(0).size(); i++){
-                result[i] = temp.get(0).get(i);
+            int[] result = new int[temp.get(j).size()];
+            for (int i = 0; i < temp.get(j).size(); i++){
+                result[i] = temp.get(j).get(i);
             }
-            intarrays.set(j, result);
+            intarrays.add(j, result);
         }
+        
+        //print items to be added
+        for (int i = 0; i < intarrays.size(); i++) {
+            String itemString = "";
+            for (int j = 0; j < intarrays.get(i).length; j++){
+                itemString += Integer.toString(intarrays.get(i)[j], x.getRadix());
+            }
+            System.out.println("Item: " + itemString);
+        }
+        
         
         //TODO add items in intarrays
         for(int i = 0; i < intarrays.size() - 1; i++){
