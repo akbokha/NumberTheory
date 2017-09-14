3c4f82a1e730f36c4fd7ffcc0cd105f03632661d
Remco <remco@192.168.2.81>
2017-09-14 10:54:29 +0200

    First start of multiplication.

--- src/PrimMultiplication.java
+++ src/PrimMultiplication.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Abdel K. Bokharouss
@@ -15,7 +19,33 @@ public class PrimMultiplication extends AbstractSolver {
     
     @Override
     public IntegerRep compute() {
+        
         // compute with x and y
+        ArrayList<ArrayList<Integer>> temp = new ArrayList<>(x.getLength());
+        
+        for (int i = x.getLength() - 1; i >= 0; i--){
+            ArrayList<Integer> temp2 = new ArrayList<>();
+            for (int j = y.getLength() - 1; j >= 0; j--){
+                int xy = x.getChars()[i] * y.getChars()[j];
+                numberOfElemMultiplications++;
+                if(temp2.size() <= j + 1 - y.getLength()){
+                    temp2.add(0, xy % x.getRadix());
+                }
+                else{
+                    temp2.set(0, temp2.get(0) + xy % x.getRadix());
+                    numberOfElemOperations++;
+                }
+                if(x.getChars()[i] * y.getChars()[j] > x.getRadix()){
+                    temp2.add(0, xy - xy % x.getRadix());
+                    numberOfElemOperations++;
+                }
+            }
+            temp.add(0, temp2);
+        }
+        
+        
+        
+        
         return new IntegerRep(0, false, new int[] {0}); // dummy instance
     }
     
