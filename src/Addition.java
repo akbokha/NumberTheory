c3404ad8b329def3dbcf9d326b8da5dfe5b08983
Remco <remco@192.168.2.81>
2017-09-14 12:24:47 +0200

    Added support for numbers with different lengths (again)

--- src/Addition.java
+++ src/Addition.java
@@ -16,6 +16,30 @@ public class Addition extends AbstractSolver {
     
     @Override
     public IntegerRep compute() {
+        
+        //If x and y do not have the same length, add leading 0s to the shortest
+        if (x.getLength() != y.getLength()) {
+            if (x.getLength() > y.getLength()) {
+                int[] temp = new int[x.getLength()];
+                int[] zeros = new int[x.getLength() - y.getLength()];
+                for (int i = 0; i < zeros.length; i++) {
+                    zeros[i] = 0;
+                }
+                System.arraycopy(zeros, 0, temp, 0, zeros.length);
+                System.arraycopy(y.getChars(), 0, temp, zeros.length, y.getLength());
+                y.setChars(temp);
+            } else {
+                int[] temp = new int[y.getLength()];
+                int[] zeros = new int[y.getLength() - x.getLength()];
+                for (int i = 0; i < zeros.length; i++) {
+                    zeros[i] = 0;
+                }
+                System.arraycopy(zeros, 0, temp, 0, zeros.length);
+                System.arraycopy(x.getChars(), 0, temp, zeros.length, x.getLength());
+                x.setChars(temp);
+            }
+        }
+        
         int[] result = new int[x.getLength() + 1];
         int remember = 0;
         int indexNumber;
